9db90e8286f3300d61f61c1a09a81a85be438071
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2022-12-09 14:20:02 +0100

    first commit

--- 1-process.py
+++ 1-process.py
@@ -72,7 +72,7 @@ def parse_sample_folder(logger, sample_folder, projectDir, sample, proposal, ses
 #            if processlib.process_files_for_run_pipeline_exist(logger, projectDir, sample, proposal, session, run, pipeline):
 #                continue
 #            logger.info('manual: ' + os.path.join(projectDir, '1-process', sample, '*', pipeline + '_*', mtz_extension))
-            for mtzfile in glob.glob(os.path.join(sample_folder, '*', mtzpath)):
+            for mtzfile in glob.glob(os.path.join(sample_folder, run, mtzpath)):
                 if processlib.process_files_for_run_pipeline_exist(logger, projectDir, sample, proposal, session, run,
                                                                    pipeline):
                     foundMTZ = True
