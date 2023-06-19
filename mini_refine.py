16c692b9dacd9223832da92b248ae360788abbbb
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2023-06-20 00:28:37 +0200

    first commit

--- mini_refine.py
+++ mini_refine.py
@@ -121,7 +121,7 @@ def run_initial_refinement(project_directory, mtzin, reference_pdb, reference_mt
     submit(n_jobs)
     for job in cmd_dict:
         if cmd_dict[job]:
-            print(cmd_dict[job])
+            os.system(cmd_dict[job])
 
 def usage():
     usage = (
