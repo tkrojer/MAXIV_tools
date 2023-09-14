8a34efe2286d8a2c2ff17a67672d84f66c72d8ea
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2023-09-14 16:18:50 +0200

    first commit

--- lib/processdb.py
+++ lib/processdb.py
@@ -230,7 +230,8 @@ def get_processing_results_for_sample(logger, dal, sample):
 #    sys.exit()
     result_list = get_result_list_of_dicts(r)
     for d in result_list:
-        print(d['cell_volume'])
+        x = d['cell_volume']
+        print(x.type, x)
 #    sys.exit()
     return result_list
 
