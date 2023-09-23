c8818006699beb325bd75954d70f4f84c7b24b4c
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2023-09-23 08:06:55 +0200

    first commit

--- 1-process.py
+++ 1-process.py
@@ -213,6 +213,7 @@ def reprocess_datasets(logger, processDir, projectDir, reprocesscsv, overwrite, 
             run = item[1]
             print('->', master_file, run)
             processlib.create_proposal_session_run_folder(logger, projectDir, sample, proposal, session, run)
+            processlib.create_pipeline_folder(logger, projectDir, sample, proposal, session, run, pipeline)
             proc_folder = processlib.get_proc_folder(projectDir, sample, proposal, session, run, pipeline)
             script_dict = processlib.add_cmd_to_script_dict(logger, script_dict, counter, pipeline, proc_dict,
                                                     proc_folder, master_file)
