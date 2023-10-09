e4a63ff52bea501ea5321655d41ec436f7b55957
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2023-10-09 15:51:43 +0200

    first commit

--- lib/refinelib.py
+++ lib/refinelib.py
@@ -162,7 +162,7 @@ def submit_jobs_to_cluster(logger, projectDir, submitList):
     os.chdir(os.path.join(projectDir, 'tmp'))
     for script in submitList:
         logger.info('submitting ' + script)
-#        os.system('sbatch ' + script)
+        os.system('sbatch ' + script)
 
 
 #def structure_cif_info(cif):
