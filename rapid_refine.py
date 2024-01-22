0e4aef9e74abacb42d740fac9bfb4644a1c10110
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2024-01-22 22:58:23 +0100

    first commit

--- rapid_refine.py
+++ rapid_refine.py
@@ -846,6 +846,7 @@ class main_window(object):
                     print('=============> looking for {0!s}'.format(emtz))
                     if os.path.isfile(emtz):
                         if emtz not in loaded_emtz:
+                            print('make_and_draw_map(emtz, "FEVENT", "PHEVENT", "1", 0, 0)')
                             imol = coot.make_and_draw_map(emtz, "FEVENT", "PHEVENT", "1", 0, 0)
                             self.mol_dict['emtz'].append(imol)
                             loaded_emtz.append(emtz)
