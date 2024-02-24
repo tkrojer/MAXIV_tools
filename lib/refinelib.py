d18366700b5c0f6392531ee8bbaa65c4d726672d
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2024-02-24 11:05:26 +0100

    first commit

--- lib/refinelib.py
+++ lib/refinelib.py
@@ -154,6 +154,7 @@ def init_refine_cmd(logger, software, projectDir, sample, mtzin, pdbref, mtzref,
         rhofit = ""
         logger.info('looking for {0!s}'.format(os.path.join(projectDir, '3-compound', sample, cpd_id + '.cif')))
         if os.path.isdir(os.path.join(projectDir, '3-compound', sample, cpd_id + '.cif')):
+            logger.info('here')
             rhofit = "-rhofit {0!s}".format(os.path.join(projectDir, '3-compound', sample, cpd_id + '.cif'))
             cmd += 'pipedream -xyzin {0!s} -hklin {1!s} -nofreeref -nolmr -d {2!s} {3!s} {4!s}'.format(pdbref, mtzin,
                                                                                                        software, lig,
