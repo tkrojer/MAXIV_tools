de78b89066c9a580fce3f345823ed40461512717
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2024-03-19 11:13:06 +0100

    first commit

--- pdf_summary.py
+++ pdf_summary.py
@@ -181,7 +181,7 @@ for index, row in df.iterrows():
         pdf.image(temp_image_path, x=image_d, y=image_y, w=image_width_mm, h=image_height_mm)
 
     # Add text cells
-    current_x = pdf.get_x() + 4 * image_width_mm
+    current_x = pdf.get_x() + 5 * image_width_mm
     pdf.set_xy(current_x, image_y)
     pdf.cell(column_widths[7], image_height_mm, str(row['reso_high']), border=1)
     pdf.cell(column_widths[8], image_height_mm, str(row['I_sigI']), border=1)
