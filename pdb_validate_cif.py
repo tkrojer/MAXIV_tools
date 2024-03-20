e77735eb9c49544452befb2d17d94b9737e12d68
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2024-03-20 12:11:47 +0100

    first commit

--- pdb_validate_cif.py
+++ pdb_validate_cif.py
@@ -1,6 +1,5 @@
 from onedep import __apiUrl__
 from onedep.api.Validate import Validate
-import time
 import getopt
 import sys
 import os
@@ -15,7 +14,7 @@ def displayStatus(sD, exitOnError=True):
             print("OneDep status: %s\n" % sD['status'])
 
 
-def validate_cif(modelFilePath, sfFilePath):
+def validate_cif(modelFilePath, sfFilePath, prefix):
     val = Validate(apiUrl=__apiUrl__)
     print("initiating new session")
     rD = val.newSession()
@@ -48,38 +47,45 @@ def validate_cif(modelFilePath, sfFilePath):
        print("[%4d] Pausing for %4d (seconds)\n" % (it, pause))
    #
    #
-    lt = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    fnR = "xray-report-%s.pdf" % lt
-#    fnR = "xray-report-%s.xml" % lt
-    rD = val.getReport(fnR)
-#    rD = val.getReportData(fnR)
+#    lt = time.strftime("%Y%m%d%H%M%S", time.localtime())
+#    fnR = "xray-report-%s.pdf" % lt
+##    fnR = "xray-report-%s.xml" % lt
+#    rD = val.getReport(fnR)
+##    rD = val.getReportData(fnR)
+
+    pdf = f"xray-report-{prefix}.pdf"
+    xml = f"xray-report-{prefix}.xml"
+    #    fnR = "xray-report-%s.xml" % lt
+    rD = val.getReport(pdf)
+    rD = val.getReportData(xml)
 
     print("finished running validation")
 
 
 def main(argv):
     #    sample_id = None
+    prefix = None
     modelFilePath = None
     sfFilePath = None
 
     try:
-        opts, args = getopt.getopt(argv, "m:s:h", ["model=", "sf=", "help"])
+        opts, args = getopt.getopt(argv, "m:s:o:h", ["model=", "sf=", "output=", "help"])
     except getopt.GetoptError:
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print("pdb_validate_cif.py -m model.cif -s sf.cif")
+            print("pdb_validate_cif.py -m model.cif -s sf.cif -o output_prefix")
             sys.exit(2)
         elif opt in ("-m", "--model"):
             modelFilePath = os.path.abspath(arg)
         elif opt in ("-s", "--sf"):
             sfFilePath = os.path.abspath(arg)
 
-    if os.path.isfile(modelFilePath) and os.path.isfile(sfFilePath):
-        validate_cif(modelFilePath, sfFilePath)
+    if os.path.isfile(modelFilePath) and os.path.isfile(sfFilePath) and prefix:
+        validate_cif(modelFilePath, sfFilePath, prefix)
     else:
-        print("pdb_validate_cif.py -m model.cif -s sf.cif")
+        print("pdb_validate_cif.py -m model.cif -s sf.cif -o output_prefix")
 
 
 if __name__ == "__main__":
