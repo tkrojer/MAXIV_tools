0153c51ff348c9b11fe4689eb81f6c772e455ce4
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2024-03-23 18:12:17 +0100

    first commit

---
+++ pdb_validate_cif.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+module load gopresto CCP4
+
+# Initialize variables for arguments
+model=""
+sf=""
+output=""
+
+# Parse arguments
+while getopts "i:o:x:" opt; do
+  case $opt in
+    m) model=$OPTARG ;;
+    s) sf=$OPTARG ;;
+    o) output=$OPTARG ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
+# Call the Python script with the parsed arguments
+ccp4-python pdb_validate_cif.py -m "$model" -s "$sf" -o "$output"
